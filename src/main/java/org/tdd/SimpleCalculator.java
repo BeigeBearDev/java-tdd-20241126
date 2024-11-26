2a6d89f16ae7481a8fdaed2ced5e8462b2475992
4est.lil.dev <carneo.dev@gmail.com>
2024-11-26 15:17:40 +0900

    commit

--- src/main/java/org/tdd/SimpleCalculator.java
+++ src/main/java/org/tdd/SimpleCalculator.java
@@ -1,7 +1,28 @@
 package org.tdd;
 
 public class SimpleCalculator {
+    // 더하기
     public int plus(int x, int y) {
         return x + y;
+    }
+
+    // 빼기
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    // 곱하기
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    // 나누기
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+    // 나머지
+    public int reminder(int x, int y) {
+        return x % y;
     }
 }
--- src/test/java/com/tdd/SimpleCalculatorTest.java
+++ src/test/java/com/tdd/SimpleCalculatorTest.java
@@ -1,19 +1,29 @@
 package com.tdd;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.tdd.SimpleCalculator;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // 테스트를 만들고 꼼수로 통과하고 리펙토링하고
 public class SimpleCalculatorTest {
     @Test
     @DisplayName("1 + 2 = 3")
-    public void testPlus() {
+    public void testPlus1() {
         SimpleCalculator calculator = new SimpleCalculator();
         int rs = calculator.plus(1,2);
 
-        assertEquals(3, rs);
+        Assertions.assertThat(rs).isEqualTo(rs == 3);
     }
+
+    @Test
+    @DisplayName("5 + 12 = 17")
+    public void testPlus2() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        int rs = calculator.plus(5,12);
+
+        Assertions.assertThat(rs).isEqualTo(rs == 17);
+    }
+
+
 }
