63572348d2d9d8573e4fba62baf3fd8611c41450
4est.lil.dev <carneo.dev@gmail.com>
2024-11-26 16:24:58 +0900

    add testPlus

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    //assertj 의존성 주입
     testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
--- src/test/java/com/tdd/SimpleCalculatorTest.java
+++ src/test/java/com/tdd/SimpleCalculatorTest.java
@@ -1,29 +1,21 @@
 package com.tdd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.tdd.SimpleCalculator;
 
-// 테스트를 만들고 꼼수로 통과하고 리펙토링하고
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// 테스트를 만들고 꼼수로 통과하고 리펙토링하고 한다.
 public class SimpleCalculatorTest {
+
     @Test
     @DisplayName("1 + 2 = 3")
     public void testPlus1() {
         SimpleCalculator calculator = new SimpleCalculator();
         int rs = calculator.plus(1,2);
 
-        Assertions.assertThat(rs).isEqualTo(rs == 3);
+        assertEquals(3, rs); //rs가 3이기를 기대한다(expected)
     }
-
-    @Test
-    @DisplayName("5 + 12 = 17")
-    public void testPlus2() {
-        SimpleCalculator calculator = new SimpleCalculator();
-        int rs = calculator.plus(5,12);
-
-        Assertions.assertThat(rs).isEqualTo(rs == 17);
-    }
-
 
 }
